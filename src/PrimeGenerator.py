675110af9fb78990c6a007d966bfa99641a159d2
Javier Luna <javierlunamolina@gmail.com>
2016-04-02 17:40:10 +0200

    Improved code in a more pythonic way
    -Python3
    -Added for..else statement
    -Added prime list. Dont need to go through all numbers, only primes

--- src/PrimeGenerator.py
+++ src/PrimeGenerator.py
@@ -1,10 +1,11 @@
-x = 1
+number = 3
+primes = [2]
 
-while True:
-	isPrime = False
-	for i in range(2, x / 2 + 1):
-		if x % i == 0:
-			isPrime = True
-			break
-	if isPrime is False: print x, "is prime."
-	x += 1
+while 1:
+    for prime in primes:
+        if not number % prime: #In python 0, empty collections and None evaluates to 0
+            break
+    else: #This is the loop's else statement. It executes if the loop runned entirely
+        primes.append(number)
+        print("Found prime number:", number)
+    number += 1
